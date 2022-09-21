a48ee7af034f6f21de0debd3a2586e1a6296da5a
Maxim Goy <maksgoy2911@gmail.com>
2022-09-21 17:15:10 +0300

    add check sum and initial payment

--- app/src/main/java/maxim/goy/lab2/MainActivity.java
+++ app/src/main/java/maxim/goy/lab2/MainActivity.java
@@ -11,24 +11,27 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar present;
     EditText sum, viewPresent, initialPayment, year;
     RadioButton typePayment;
-    TextView overpayment, viewSum, generalSumPayment, presentOverpayment, avePay, viewAvePay;
+    TextView overpayment, viewSum, generalSumPayment, presentOverpayment, avePay, viewAvePay,
+            incorrectMessage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        present = findViewById(R.id.procent);
+        present = findViewById(R.id.present);
         sum = findViewById(R.id.sum);
-        viewPresent = findViewById(R.id.viewProcent);
+        viewPresent = findViewById(R.id.viewPresent);
         initialPayment = findViewById(R.id.initialPayment);
         year = findViewById(R.id.year);
         typePayment = findViewById(R.id.differentiated);
+        incorrectMessage = findViewById(R.id.incorrectMessage);
 
         overpayment = findViewById(R.id.overpayment);
         viewSum = findViewById(R.id.viewSum);
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         avePay = findViewById(R.id.avePay);
         viewAvePay = findViewById(R.id.textView9);
 
+        //region listeners
         sum.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -45,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = sum.getText().toString();
+                if (str.equals("")) {
+                    sum.setText("0");
+                    return;
+                }
+                if (str.toCharArray()[0] == '0' && str.length() > 1)
+                    sum.setText(str.substring(1));
+                checkSumAndInitialPayment();
             }
 
             @Override
@@ -76,6 +88,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = viewPresent.getText().toString();
+                if (str.equals("")) {
+                    viewPresent.setText("0");
+                    return;
+                }
+                if (str.toCharArray()[0] == '0' && str.length() > 1)
+                    viewPresent.setText(str.substring(1));
                 present.setProgress(Integer.parseInt(String.valueOf(viewPresent.getText())), true);
             }
 
@@ -84,11 +103,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        initialPayment.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = sum.getText().toString();
+                if (str.equals("")) {
+                    sum.setText("0");
+                    return;
+                }
+                if (str.toCharArray()[0] == '0' && str.length() > 1)
+                    sum.setText(str.substring(1));
+                checkSumAndInitialPayment();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        //endregion
 
     }
 
     @SuppressLint("SetTextI18n")
     public void calculateCredit(View view) {
+        Toast toast = Toast.makeText(this, "Hello Android!", Toast.LENGTH_LONG);
+        toast.show();
         float sum = Float.parseFloat(String.valueOf(this.sum.getText()));
         Credit.typePayment typePay = this.typePayment.isChecked() ?
                 Credit.typePayment.Differentiated : Credit.typePayment.Annuity;
@@ -109,5 +154,14 @@ public class MainActivity extends AppCompatActivity {
             avePay.setVisibility(View.INVISIBLE);
             viewAvePay.setVisibility(View.INVISIBLE);
         }
+    }
+
+    public void checkSumAndInitialPayment() {
+        int sum = Integer.parseInt(this.sum.getText().toString());
+        int initialPayment = Integer.parseInt(this.initialPayment.getText().toString());
+        if (sum < initialPayment) {
+            incorrectMessage.setVisibility(View.VISIBLE);
+        } else
+            incorrectMessage.setVisibility(View.INVISIBLE);
     }
 }
